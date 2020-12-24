251091f3b885244cf6ad56b363f4a05d5a35b9cc
lumenghe <lumenghe@gmail.com>
2020-12-24 07:19:30 +0100

    feat(app): add main()

--- url_shortener/app.py
+++ url_shortener/app.py
@@ -15,5 +15,11 @@ app = Flask(__name__)
 shortener = UrlShortener()
 logger = logging.getLogger(__name__)
 
+def main():
+    """ main function """
+    logging.basicConfig(level=logging.INFO)
+    app.run(port=os.getenv("APP_PORT"), debug=True)
+
+
 if __name__ == "__main__":
     main()
