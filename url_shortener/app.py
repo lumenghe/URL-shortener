b30a7782ba1c99d1ca1107bbcb5afb3146f4e674
lumenghe <lumenghe@gmail.com>
2020-12-24 07:17:30 +0100

    feat(app): add import in app file

--- url_shortener/app.py
+++ url_shortener/app.py
@@ -1,4 +1,11 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """ app main file """
 import datetime
+import logging
+import os
+import urllib
 
+from flask import Flask, jsonify, redirect, request
+
+from url_shortener import DBHandler, UrlShortener
