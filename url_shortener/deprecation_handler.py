f508ad0d64fa6d38ede76f3bcf5ed5fe74cf13e5
lumenghe <lumenghe@gmail.com>
2020-12-31 13:52:46 +0100

    feat(deprecation_handler): add class DeprecationHandler staticmethod

--- url_shortener/deprecation_handler.py
+++ url_shortener/deprecation_handler.py
@@ -14,3 +14,14 @@ class DeprecationHandler:
     """ deprecation handler class """
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
+
+    @staticmethod
+    def deprecation_rows():
+        """deprecation rows
+        delete rows whose updated time < now - delta
+        :return: None
+        """
+        db_handler = DBHandler()
+        delta = datetime.timedelta(seconds=DELTA)
+        record = db_handler.deprecation((datetime.datetime.now() - delta,))
+        logger.info("delete {} row".format(re
