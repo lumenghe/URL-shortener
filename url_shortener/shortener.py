3d633452c628e69262c28de1f7cbacc0ad925000
lumenghe <lumenghe@gmail.com>
2021-01-03 06:11:12 +0100

    feat(shortener): add shortener factory for urlshortener class

--- url_shortener/shortener.py
+++ url_shortener/shortener.py
@@ -15,3 +15,12 @@ class UrlShortener:
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
 
+    def shortener_factory(self, url):
+        """factory which builds shortener
+        :param url: original url
+        :return: shorter url
+        """
+        hashed = blake2b(url.encode("utf-8"), digest_size=self.DIGEST_SIZE)
+        base64_bytes = base64.b64encode(hashed.digest(), altchars=b"@~")
+        base64_message = base64_bytes.decode("utf-8")
+        return base64_message
