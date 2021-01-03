42b45ccbcd82a8878fd1f76821588d71799ce2fb
lumenghe <lumenghe@gmail.com>
2021-01-03 06:12:11 +0100

    chore(style): make style

--- url_shortener/shortener.py
+++ url_shortener/shortener.py
@@ -6,11 +6,11 @@ from hashlib import blake2b
 
 logger = logging.getLogger(__name__)
 
+
 class UrlShortener:
     """class of generating a 8 bytes hash shorter url."""
 
     DIGEST_SIZE = 8
-
 
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
