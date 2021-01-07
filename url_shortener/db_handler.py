828cbcf967493486c7f73a7b06c4b48060061141
lumenghe <lumenghe@gmail.com>
2021-01-07 09:23:01 +0100

    feat(db_handler): add DBHandler init

--- url_shortener/db_handler.py
+++ url_shortener/db_handler.py
@@ -2,11 +2,16 @@
 """Database Handler class"""
 import logging
 import os
+
 import psycopg2
+
 from url_shortener import sql
 
 logger = logging.getLogger(__name__)
 
+
 class DBHandler:
     """class of Database Handler"""
-    pass
+
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO)"ok")
